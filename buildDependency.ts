6705845f73148d5125a974e8df581294d474b3b8
Antonio Stoilkov <antonio.stoilkov@gmail.com>
2021-02-03 17:38:42 +0200

    🧪 next iteration

--- buildDependency.ts
+++ buildDependency.ts
@@ -1,17 +1,30 @@
 import { join } from 'path'
 import { promises } from 'fs'
 import { build } from 'esbuild'
-import { PackageJson } from 'type-fest'
+import { LiteralUnion, PackageJson } from 'type-fest'
 
-export default async function buildDependency(dependencyDirPath: string): Promise<number> {
+export default async function buildDependency(
+  dependencyDirPath: string,
+  importStatement: LiteralUnion<'default', string>,
+): Promise<string> {
   const packageJson: PackageJson = JSON.parse(
     await promises.readFile(join(dependencyDirPath, 'package.json'), { encoding: 'utf-8' }),
   )
+
+  const entryFilePath = join(dependencyDirPath, '__entry__.js')
+
+  await promises.writeFile(
+    entryFilePath,
+    importStatement === 'default'
+      ? `import defaultImport from './index'\nexport default defaultImport`
+      : `import { ${importStatement} } from './index'\nexport default ${importStatement}`,
+  )
+
   const outputPath = join(dependencyDirPath, '__output__.js')
 
   await build({
     outfile: outputPath,
-    entryPoints: [join(dependencyDirPath, 'index.js')],
+    entryPoints: [entryFilePath],
     bundle: true,
     minify: true,
     define: {
@@ -20,5 +33,5 @@ export default async function buildDependency(dependencyDirPath: string): Promis
     external: Object.keys(packageJson.peerDependencies ?? {}),
   })
 
-  return (await promises.stat(outputPath)).size
+  return outputPath
 }
--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import buildDependency from './buildDependency'
 import installDependency from './installDependency'
 import prettyBytes from 'pretty-bytes'
+import { statSync } from 'fs'
+import simpleSpawn from 'simple-spawn'
 ;(async () => {
   try {
-    console.log(
-      prettyBytes(await buildDependency(await installDependency('use-local-storage-state'))),
-    )
+    const dependencyDirPath = await installDependency('use-local-storage-state', '7.0.0')
+    simpleSpawn('code', [dependencyDirPath])
+    const outputPath = await buildDependency(dependencyDirPath, 'default')
+    console.log(prettyBytes(statSync(outputPath).size))
   } catch (err) {
     throw err
   }
